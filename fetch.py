cd283eca408aedf293ff0ed48f2f6bd4fd0b1790
Akshay Amrutkar <76642867+akshay48-web@users.noreply.github.com>
2020-12-30 22:08:36 -0800

    Add files via upload

---
+++ fetch.py
@@ -0,0 +1,22 @@
+ import program as pro
+
+pro.create("maitri",56)
+
+pro.create("src",10,9098) 
+
+pro.read("oliver")
+
+pro.read("team")
+
+pro.create("maitri",40)
+ 
+pro.delete("maitri")
+
+t1=Thread(target=(create or read or delete),args=(key_name,value,timeout)) 
+t1.start()
+t1.sleep()
+
+t2=Thread(target=(create or read or delete),args=(key_name,value,timeout)) 
+t2.start()
+t2.sleep()
+ # up to tn
